20a26c09416deaf98ef6abeee8259933114b1e0b
JKxKookie <mariaguydefontgalland@gmail.com>
2022-05-13 15:32:04 +0200

    component subproduct

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,4 @@
 <Header></Header>
 <main>
 	<!-- MAIN -->
-	<div>
-		<p class="title">Egg'n Tea Timer</p>
-		<p class="description">Make yourself a Tea or an Egg very easily with our instructions</p>
-		<div class="productsType">
-		</div>
-
-		<div id="myData"></div>
-	</div>
 </main>
