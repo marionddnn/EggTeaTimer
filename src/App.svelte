354e29828352a8ec47ed46e531db60da85999ee6
JKxKookie <mariaguydefontgalland@gmail.com>
2022-05-13 15:32:04 +0200

    component subproduct

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,34 @@
 <script>
 	import Header from './components/Header.svelte';
+
 </script>
 
 <Header></Header>
 <main>
 	<!-- MAIN -->
 </main>
+	<div>
+		<p class="title">Egg'n Tea Timer</p>
+		<p>Make yourself a Tea r an Egg ery easily with our instructions</p>
+	</div>
+
+<style>
+	main {
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	.title {
+		color: #04543F;
+		font-size: 20px;
+		font-weight: bold;
+		text-align: center;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Products.svelte
@@ -0,0 +1,15 @@
+<script>
+    import ProductsType from './components/ProductsType.svelte';
+</script>
+
+<main>
+    <div class="products">
+        <div>
+            <p><a href="ProductsType.svelte">Produits</a></p>
+        </div>
+    </div>
+</main>
+
+<style>
+
+</style>
