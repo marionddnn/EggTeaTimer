25d557118715bf6fe36653911baf16da680da07a
JKxKookie <mariaguydefontgalland@gmail.com>
2022-05-29 14:23:15 +0200

    json traduction & ProductsTypeComponent fini

--- src/components/Products.svelte
+++ src/components/Products.svelte
@@ -1,15 +1,40 @@
 <script>
-    import ProductsType from './components/ProductsType.svelte';
 </script>
 
 <main>
     <div class="products">
         <div>
-            <p><a href="ProductsType.svelte">Produits</a></p>
+            <div id="product-egg">
+                <p class="productTitle">Eggs</p>
+            </div>
+
+            <div id="product-tea">
+                <p class="productTitle">Tea</p>
+            </div>
         </div>
     </div>
 </main>
 
 <style>
+
+    #product-egg, #product-tea {
+        border-radius: 20px;
+        padding: 4px 24px;
+        margin: auto;
+        width: fit-content;
+    }
+
+    .productTitle {
+        font-weight: bold;
+    }
+
+    #product-egg{
+        background-color: #fbe1b5;
+        margin-bottom: 20px;
+    }
+
+    #product-tea{
+        background-color: #d4edd3;
+    }
 
 </style>
