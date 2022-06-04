f3c8842fa610804d4aff972065133dd23afc4b00
lucas <luqui31@gmail.com>
2022-06-04 09:04:00 +0200

    Stater pack by lulu ;)

---
+++ src/components/Header.svelte
@@ -0,0 +1,4 @@
+<header class="text-center my-4">
+    <h1 class="text-green title fw-bold">Egg’n Tea Timer</h1>
+    <h4>Make yourself a Tea or an Egg very easily with our instructions</h4>
+</header>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import App from './App.svelte';
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+var app = new App({
+	target: document.body
 });
 
 export default app;
