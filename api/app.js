23ce6222b5f11ad22c1ea517709415baafd59d13
JKxKookie <mariaguydefontgalland@gmail.com>
2022-06-04 19:18:49 +0200

    recup data from json & create API

---
+++ api/app.js
@@ -0,0 +1,27 @@
+const data = require ("../data.json");
+const dataParsed = JSON.parse(JSON.stringify(data));
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors')
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send(data);
+});
+
+app.get("/products/:type", (req, res) => {
+
+    let dataJson = Object.values(dataParsed);
+    let array = dataJson.find(item=>item);
+    //console.log(array.find(item => item.title === req.params.type));
+    let result = array.find(item => item.title === req.params.type);
+    //console.log(dataJson.categories.find(item => item.title === req.params.type));
+    res.send(result);
+});
+
+app.listen(8081, () => {
+    console.log("App's running on port 8081");
+});
