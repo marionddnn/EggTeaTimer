446881f8b7a5f45583c82c6cdc08c24984de03c5
JKxKookie <mariaguydefontgalland@gmail.com>
2022-06-04 15:35:31 +0200

    fix bug rebase 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,16 @@
 <script>
-<<<<<<< HEAD
 	import Header from './components/Header.svelte';
-=======
-	import Products from "./components/Products.svelte";
-	//fetch("people.json").then(response => response.json()).then(json => console.log(json));
->>>>>>> data json trad
 </script>
 
 <Header></Header>
 <main>
-<<<<<<< HEAD
 	<!-- MAIN -->
-=======
 	<div>
 		<p class="title">Egg'n Tea Timer</p>
 		<p class="description">Make yourself a Tea or an Egg very easily with our instructions</p>
 		<div class="productsType">
-			<Products/>
 		</div>
 
 		<div id="myData"></div>
 	</div>
->>>>>>> data json trad
 </main>
