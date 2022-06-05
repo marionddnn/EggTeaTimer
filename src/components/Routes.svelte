79ed748ca38aff60fc762641e11a84d44ba55a18
marion dieudonné <m.j.dieudonne@orange.fr>
2022-06-05 10:42:27 +0200

    move Routes component

---
+++ src/components/Routes.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Router, Route } from "svelte-routing";
+    import Recipe from "./components/Recipe.svelte";
+    import Categories from "./components/Categories.svelte";
+    import RecipesList from "./components/RecipesList.svelte";
+    export let url = "";
+</script>
+
+<main>
+  <Router url="{url}">
+      <div>
+        <Route path="recipe/:id" component="{Recipe}" />
+        <Route path="recipes/:type" component="{RecipesList}" />
+        <Route path="/" component="{Categories}"/>
+      </div>
+  </Router>
+</main>
