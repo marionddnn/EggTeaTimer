e76337d9d4927e4e80a19657dc6544fe8509cc53
marion dieudonné <m.j.dieudonne@orange.fr>
2022-06-06 12:09:42 +0200

    fix conflicts on the main branch

--- src/components/Categories.svelte
+++ src/components/Categories.svelte
@@ -5,67 +5,28 @@
     import { navigate } from "svelte-routing";
     import { Link } from "svelte-routing";
 
-<<<<<<< HEAD
-    function onSubmit() {
-        login().then(() => {
-            navigate("/success", { replace: true });
-        });
-    }
-    let list = JSON.parse(JSON.stringify(json));
-    const types = [];
-    for (const category of list.categories) {
-        types.push({ name: category.title, url: "/recipes/" + category.title });
-    }
-=======
->>>>>>> 0581ef18bf4609556a11a6cd3fbc87d0de9f9b99
 
     let api = [];
     let baseUrl = "/recipes/";
     onMount(async () => {
-<<<<<<< HEAD
-        await fetch("http://localhost:8081/")
-            .then((r) => r.json())
-            .then((data) => {
-                api = JSON.parse(JSON.stringify(data));
-                //api = data;
-                //console.log(api);
-            });
-    });
-=======
         await fetch('http://localhost:8081/').then(r => r.json()).then(data => {
              api = JSON.parse(JSON.stringify(data));
         });
     })
 
->>>>>>> 0581ef18bf4609556a11a6cd3fbc87d0de9f9b99
 </script>
 
 <div class="products">
     <div>
-<<<<<<< HEAD
         <p class="categorie">Categories :</p>
-        <!--{#each types as type}
-        <a use:link href={type.url} on:click={handleClick}>
-            {type.name}
-        </a>
-        {/each} -->
-
-        {#each Object.values(api) as item}
-            {#each item as category}
-                <a use:link href={baseUrl + category.title}>
-                    {category.title}
-                    <img src="../../img/icons8-tea-bag-16.png" alt="" />
-                    <img src="../../img/icons8-oeuf-au-plat-16.png" alt="" />
-                </a>
-=======
-        <p> Categories : </p>
 
         {#each  Object.values(api) as item }
             {#each  item as category }
             <a use:link href={baseUrl+category.title}>
                 {category.title}
+                <img src="../../img/icons8-tea-bag-16.png" alt="" />
+                <img src="../../img/icons8-oeuf-au-plat-16.png" alt="" />
             </a>
->>>>>>> 0581ef18bf4609556a11a6cd3fbc87d0de9f9b99
             {/each}
         {/each}
     </div>
